7c241a663a582daa7ee5b2b3785464f3d2354be9
Travis Scotto <travisscotto@MacBook-Pro.local>
2021-10-21 19:13:18 -0700

    Config update

--- lesson-4-deploying-terraform-assets/demo/_config.tf
+++ lesson-4-deploying-terraform-assets/demo/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-tscotto6"
+     bucket = "udacity-tf-tscotto7"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
