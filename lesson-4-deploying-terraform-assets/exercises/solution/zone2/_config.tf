15fb9ba757912a45d7ba936cf9fe66b6f862b5f5
Travis Scotto <travisscotto@Traviss-MBP-2.localdomain>
2021-11-05 16:46:12 -0400

    Fixing AMI

--- lesson-4-deploying-terraform-assets/exercises/solution/zone2/_config.tf
+++ lesson-4-deploying-terraform-assets/exercises/solution/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tscotto3"
+     bucket = "udacity-tf-tscotto-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
--- lesson-4-deploying-terraform-assets/exercises/solution/zone2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/solution/zone2/ec2.tf
@@ -3,7 +3,7 @@
    instance_count     = 2
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0d382e80be7ffdae5"
+   aws_ami            = "ami-0e0bf4b3a0c0e0adc"
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
