6253a870064ea02797832a9b58d5f48a126b266f
Marta Raczynska <marta.raczynska@vimn.com>
2022-05-26 17:36:12 +0200

    Change ami id

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis-west"
+     bucket = "udacity-tf-marta-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
--- project/starter-code/zone2/ec2.tf
+++ project/starter-code/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0078038b37a8646b2"
+   aws_ami            = "ami-090e21af41b576a07"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
    public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
