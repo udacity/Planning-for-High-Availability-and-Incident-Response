599b31a56db195456597009f45c5134a79249694
SNAKE169 <ducanh6901@gmail.com>
2022-12-17 10:25:40 +0900

    Update _config.tf

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-<user_name>"
+     bucket = "udacity-tf-anhbd3"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
