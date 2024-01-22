a45578217ddc42e113537f6bdf40fcb23a641ecc
Rafał Szymaniuk <rszymani@gmail.com>
2024-01-22 19:41:17 +0100

    Update _config.tf update bucket

--- lesson-2-sli-slo/exercises/starter/_config.tf
+++ lesson-2-sli-slo/exercises/starter/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-rszymani"
+     bucket = "udacity-tf-rszymani1"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
