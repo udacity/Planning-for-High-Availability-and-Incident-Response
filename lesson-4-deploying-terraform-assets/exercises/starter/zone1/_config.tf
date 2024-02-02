46e67528734e0b1c8409a6d44ce2aace1232c5ea
rafal.szymaniuk <rszymaniuk@gmail.com>
2024-02-02 12:35:46 +0100

    chore(lesson4): update bucket

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-tscotto6"
+     bucket = "udacity-tf-rszymani"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
