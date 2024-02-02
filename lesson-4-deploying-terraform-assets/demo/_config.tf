440cb1a9f4869fcf271cf58ff6d7e0b6f66c9e85
rafal.szymaniuk <rszymaniuk@gmail.com>
2024-02-02 11:44:54 +0100

    chore(_config.tf): change name of the udacity-tf-rszymani

--- lesson-4-deploying-terraform-assets/demo/_config.tf
+++ lesson-4-deploying-terraform-assets/demo/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-tscotto-west"
+     bucket = "udacity-tf-rszymani"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
